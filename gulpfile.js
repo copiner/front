d9304042312c69900142b09aeef28196684b9111
copiner <810023494@qq.com>
2022-05-19 10:34:09 +0800

    mobile

--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,7 @@
 const { src, dest, task, series, parallel, watch, lastRun } = require('gulp');
 const connect = require('gulp-connect');
 const del = require('del');
-// const sass = require('gulp-sass');
 
-task('rem', function (cb) {
-  src('src/mobile/rem/scss/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(dest('dist/mobile/rem/css/'))
-  cb();
-});
-
-task('vw', function (cb) {
-  src('src/mobile/vw/scss/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(dest('dist/mobile/vw/css/'))
-  cb();
-});
-
-task('vwrem', function (cb) {
-  src('src/mobile/vwrem/scss/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(dest('dist/mobile/vwrem/css/'))
-  cb();
-});
-
-task('mi', function (cb) {
-  src('src/mobile/mi/scss/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(dest('dist/mobile/mi/css/'))
-  cb();
-});
-
-// task('source', series('vw','rem','vwrem','mi', function (cb) {
-//     src('src/**/*')
-//     .pipe(dest('dist/'))
-//     .pipe(connect.reload());
-//     cb();
-// }));
 
 task('source', function (cb) {
     src('src/**/*')
